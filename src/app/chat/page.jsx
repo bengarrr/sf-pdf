3db78a1e50a165febf794b0138a117ac197c622e
bengarrr <udlrsc@gmail.com>
2025-10-06 01:26:30 -0700

    ...

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -5,21 +5,24 @@ import PDFViewer from "@/components/viewer";
 import ProtectedRoute from "@/components/protected";
 import { getSession } from "@/lib/auth/session";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Page({ params }) {
     const session = getSession();
     const chatId = useSearchParams().get("chatId");
 
     return (
-        <ProtectedRoute>
-            <div className="flex flex-row">
-                <div className="w-full lg:w-1/2">
-                    <PDFViewer></PDFViewer>
+        <Suspense>
+            <ProtectedRoute>
+                <div className="flex flex-row">
+                    <div className="w-full lg:w-1/2">
+                        <PDFViewer></PDFViewer>
+                    </div>
+                    <div className="w-full lg:w-1/2">
+                        <Chatbox currentUserId={session?.user.id} chatId={chatId}></Chatbox>
+                    </div>
                 </div>
-                <div className="w-full lg:w-1/2">
-                    <Chatbox currentUserId={session?.user.id} chatId={chatId}></Chatbox>
-                </div>
-            </div>
-        </ProtectedRoute>
+            </ProtectedRoute>
+        </Suspense>
     )
 }
